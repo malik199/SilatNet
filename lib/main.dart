43914c839a3bfaa71c0a490aebd3690b082168de
malik199 <malik199@gmail.com>
2021-03-23 21:10:46 -0400

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'colors.dart';
+import 'rss_reader.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        // Changed my theme to look more like a terminal. Colors defined in
+        // colors.dart
+        brightness: Brightness.light,
+      ),
+      home: RSSReader(),
+    );
+  }
+}
